ffc3fea5e685e6b363fb2be970c592aceee0eb38
Felix Boelter <felix.boelter@googlemail.com>
2025-07-04 15:19:34 +0200

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -8,7 +8,7 @@ apt-get install software-properties-common wget curl \
 
 # install Python packages
 python3 -m pip install --upgrade pip
-pip3 install --user -r .devcontainer/requirements.txt
+pip3 install -r .devcontainer/requirements.txt
 
 # update CUDA Linux GPG repository key
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
@@ -24,13 +24,15 @@ apt-get install libcudnn8=8.9.7.*-1+cuda12.1 -y
 apt-get install libcudnn8-dev=8.9.7.*-1+cuda12.1 -y
 
 # install exact NVIDIA driver libraries to match host kernel driver 570.133.20
-apt-get update
 apt-get install -y \
   libnvidia-compute-570=570.133.20-0ubuntu1 \
   libnvidia-decode-570=570.133.20-0ubuntu1 \
   nvidia-utils-570=570.133.20-0ubuntu1
+
+# Remove duplicate CUDA source-list (silences apt warning)
 rm /etc/apt/sources.list.d/archive_uri-https_developer_download_nvidia_com_compute_cuda_repos_ubuntu2204_x86_64_-jammy.list
 apt-get update
+
 # install recommended packages
 apt-get install zlib1g g++ freeglut3-dev \
     libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
